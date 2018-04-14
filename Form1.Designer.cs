994c69456ed94f4ab3f6c5dfb13ea2646d41a818
Thierry Bernier <guillaumeportails@users.noreply.github.com>
2018-04-14 17:15:11 +0200

    Save

---
+++ Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace CFDslurp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose (bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose ();
+            }
+            base.Dispose (disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent ()
+        {
+            this.textBoxURL = new System.Windows.Forms.TextBox ();
+            this.button1 = new System.Windows.Forms.Button ();
+            this.webBrowser = new System.Windows.Forms.WebBrowser ();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer ();
+            this.button4 = new System.Windows.Forms.Button ();
+            this.button3 = new System.Windows.Forms.Button ();
+            this.button2 = new System.Windows.Forms.Button ();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit ();
+            this.splitContainer1.Panel1.SuspendLayout ();
+            this.splitContainer1.Panel2.SuspendLayout ();
+            this.splitContainer1.SuspendLayout ();
+            this.SuspendLayout ();
+            // 
+            // textBoxURL
+            // 
+            this.textBoxURL.Location = new System.Drawing.Point (8, 10);
+            this.textBoxURL.Name = "textBoxURL";
+            this.textBoxURL.Size = new System.Drawing.Size (410, 20);
+            this.textBoxURL.TabIndex = 0;
+            this.textBoxURL.Text = "http://parapente.ffvl.fr/cfd/liste/2010?page=";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point (505, 8);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size (75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Add IGC";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler (this.button1_Click);
+            // 
+            // webBrowser
+            // 
+            this.webBrowser.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser.Location = new System.Drawing.Point (0, 0);
+            this.webBrowser.MinimumSize = new System.Drawing.Size (20, 20);
+            this.webBrowser.Name = "webBrowser";
+            this.webBrowser.Size = new System.Drawing.Size (755, 481);
+            this.webBrowser.TabIndex = 2;
+            this.webBrowser.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler (this.webBrowser_DocumentCompleted);
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
+            this.splitContainer1.Location = new System.Drawing.Point (0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add (this.button4);
+            this.splitContainer1.Panel1.Controls.Add (this.button3);
+            this.splitContainer1.Panel1.Controls.Add (this.button2);
+            this.splitContainer1.Panel1.Controls.Add (this.textBoxURL);
+            this.splitContainer1.Panel1.Controls.Add (this.button1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add (this.webBrowser);
+            this.splitContainer1.Size = new System.Drawing.Size (755, 533);
+            this.splitContainer1.SplitterDistance = 48;
+            this.splitContainer1.TabIndex = 3;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point (668, 8);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size (75, 23);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Clear";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler (this.button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point (586, 8);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size (75, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Get IGC";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler (this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point (424, 8);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size (75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "GOTO";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler (this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF (6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size (755, 533);
+            this.Controls.Add (this.splitContainer1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.splitContainer1.Panel1.ResumeLayout (false);
+            this.splitContainer1.Panel1.PerformLayout ();
+            this.splitContainer1.Panel2.ResumeLayout (false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit ();
+            this.splitContainer1.ResumeLayout (false);
+            this.ResumeLayout (false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxURL;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.WebBrowser webBrowser;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using System.Net;
+using System.IO;
+using System.Diagnostics;
+
+namespace CFDslurp
+{
+    public partial class Form1 : Form
+    {
+        public Form1 ()
+        {
+            InitializeComponent ();
+
+            webBrowser.AllowNavigation = true;
+            webBrowser.AllowWebBrowserDrop = false;
+            mIGC.Clear ();
+        }
+
+        enum State
+        {
+            Nil,
+            ScanningGPSLinks,
+            ScanningIGCLink
+        };
+
+        State mState = State.Nil;
+        int mPage = 0;
+
+        // Liste de pages ou trouver des IGC : les HREF "gps"
+        List<string> mList = new List<string> ();
+
+        //
+        HashSet<string> mIGC = new HashSet<string> ();
+
+        // Enumeration des pages a explorer (passer a Next a chaque PageLoadCompleted)
+        IEnumerator<string> mEnum = null;
+
+        // GOTO one page
+        private void button2_Click (object sender, EventArgs e)
+        {
+            mState = State.ScanningGPSLinks;
+            mPage = 0;
+            mList.Clear ();
+            webBrowser.Navigate (textBoxURL.Text + mPage);
+        }
+
+        // ADD IGC : scanne les liens "gps" de la page en cours
+        private void button1_Click (object sender, EventArgs e)
+        {
+            mState = State.ScanningIGCLink;
+            Trace.WriteLine ("Searching IGC");
+            mEnum = mList.GetEnumerator ();
+            if (mEnum.MoveNext ()) webBrowser.Navigate (mEnum.Current);
+        }
+
+        // Cherche les liens HREF "gps" dans le document et ajoute ces pages a mList
+        // Chacune d'elles contient en effet un HREF vers un IGC
+        private void ScanGPSLinks (HtmlDocument dom)
+        {
+            foreach (HtmlElement lnk in dom.Links)
+            {
+                //Trace.WriteLine ("Name=" + lnk.Name + " Tag=" + lnk.TagName + " Id="+lnk.Id + " Out=" + lnk.OuterText);
+                if (lnk.OuterText == "gps")
+                {
+                    //Trace.WriteLine ("***  " + lnk.GetAttribute ("HREF"));
+                    mList.Add (lnk.GetAttribute ("HREF"));
+                }
+            }
+        }
+
+        // Cherche le lien HREF "IGC" dans le document et ajoute cette pages a mIGC
+        private void ScanIGCLink (HtmlDocument dom)
+        {
+            // Find the link to the IGC file
+            foreach (HtmlElement lnk in dom.Links)
+            {
+                //Trace.WriteLine ("Name=" + lnk.Name + " Tag=" + lnk.TagName + " Id=" + lnk.Id + " Out=" + lnk.OuterText);
+                if ((lnk.OuterText != null) && (lnk.OuterText.EndsWith (".igc")))
+                {
+                    //Trace.WriteLine ("***  " + lnk.GetAttribute ("HREF"));
+                    mIGC.Add (lnk.GetAttribute ("HREF"));
+                }
+            }
+        }
+
+        // Le browser a fini de charger la page en cours
+        private void webBrowser_DocumentCompleted (object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            webBrowser.Stop ();                         // Arrete les scripts inutiles !
+            HtmlDocument dom = webBrowser.Document;
+            Trace.WriteLine ("Gone to " + dom.Url);
+
+            switch (mState)
+            {
+                case State.Nil :
+                    break;
+
+                case State.ScanningGPSLinks:
+                    ScanGPSLinks (dom);
+
+                    // Navigate to the next page
+                    ++mPage;
+                    webBrowser.Navigate (textBoxURL.Text + mPage);
+                    break;
+
+                case State.ScanningIGCLink:
+                    if (mEnum == null) return;
+                    ScanIGCLink (dom);
+
+                    // Navigate to the next page
+                    if (!mEnum.MoveNext ())
+                    {
+                        Trace.WriteLine ("Listing DONE");
+                        mEnum = null;
+                    }
+                    else
+                        webBrowser.Navigate (mEnum.Current);
+                break;
+            }
+        }
+
+        // GET IGC
+        private void button3_Click (object sender, EventArgs e)
+        {
+            mEnum = null;
+            mState = State.Nil;
+            string Home = "C:/GIS/IGC/";
+
+            Directory.CreateDirectory (Home);
+
+            WebClient wc = new WebClient ();
+            foreach (string igc in mIGC)
+            {
+                Trace.WriteLine ("Downloading " + Path.GetFileName (igc));
+                wc.DownloadFile (igc, Home + "/" + Path.GetFileName (igc));
+            }
+            Trace.WriteLine ("Downloading DONE");
+        }
+
+        private void button4_Click (object sender, EventArgs e)
+        {
+            mIGC.Clear ();
+        }
+
+    }
+}
